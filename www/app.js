7bfcc6221e583cac5e8bfc8c0bf2660e34f7fd50
csouchet <souchet.celine.5@gmail.com>
2015-04-27 14:46:50 +0200

    - Put the tab panel on the bottom

--- www/app.js
+++ www/app.js
@@ -9,16 +9,20 @@
 // 'org.beontime.features' is found in features.js
 angular.module('starter', ['ionic', 'org.beontime.features'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-	// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-	// for form inputs)
-	if (window.cordova && window.cordova.plugins.Keyboard) {
-	  cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-	}
-	if (window.StatusBar) {
-	  // org.apache.cordova.statusbar required
-	  StatusBar.styleDefault();
-	}
-  });
-});
+    .config(['$ionicConfigProvider', function ($ionicConfigProvider) {
+        $ionicConfigProvider.tabs.position('bottom');
+    }])
+    
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    });
